840323fdd289aff20e1717f42dcba711e3a40f15
Camilaf19 <cam.fonseca19@gmail.com>
2023-03-05 20:53:17 -0500

    Fixing the board bug

--- src/classes/cells.ts
+++ src/classes/cells.ts
@@ -18,54 +18,46 @@ export class Cell extends Board {
     if (Board.cells[this.rows][this.cols] !== 0) {
       return 0
     }
+
     this.neighbors.forEach((direction) => {
-      let row = this.rows + direction[0]
+      let row = this.rows + direction[0] // posicion adayacentes
       let col = this.cols + direction[1]
 
-      if (
-        Board.cells[row][col] === 3 - this.currentPlayer && //la celda adyacente sea el oponente
-        row >= 0 &&
-        row < 8 &&
-        col >= 0 &&
-        col < 8
-      ) {
-        Board.cells[this.rows][this.cols] = this.currentPlayer //pintar el turno actual
-        this.flipTokens(
-          this.rows,
-          this.cols,
-          direction,
-          this.currentPlayer,
-          Board
-        )
-
-        //  Board.cells[this.rows][this.cols] = this.currentPlayer //pintar el turno actual
-      //  Board.cells[row][col] = this.currentPlayer // pintar la celda adyacente 
-
+      if (row >= 0 && row < 8 && col >= 0 && col < 8) {
+        if (Board.cells[row][col] === 3 - this.currentPlayer) {  // la celda adyacente sea el oponente
+          Board.cells[this.rows][this.cols] = this.currentPlayer //pintar el turno actual
+          this.flipTokens(
+            this.rows,
+            this.cols,
+            direction,
+            this.currentPlayer,
+            Board
+          )
+        }
       }
-
-      return Board.cells[this.rows][this.cols]
     })
+
+    return Board.cells[this.rows][this.cols]
   }
 
   flipTokens(
-    row: number,
+    row: number, // posicion donde jugador quiere poner su ficha
     col: number,
-    direction: number[], //valores adyacentes del actual
+    direction: number[],
     currentPlayer: number,
     Board: Board
   ) {
-    let r = row + direction[0]
+    let r = row + direction[0] // adyacentes del actual
     let c = col + direction[1]
     while (
-     r >= 0 &&
+      r >= 0 &&
       r < 8 &&
       c >= 0 &&
-      c < 8 && 
+      c < 8 &&
       Board.cells[r][c] === 3 - currentPlayer
     ) {
-      debugger
-      Board.cells[r][c] = currentPlayer
-      r += direction[0]
+      Board.cells[r][c] = currentPlayer // pinta la celda adaycente del jugador actual
+      r += direction[0] //cambia a los valores adayacentes de la adyacente
       c += direction[1]
     }
   }
